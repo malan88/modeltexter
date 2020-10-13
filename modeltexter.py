9f7549a756d2b8ca0b1aeccdc87a340cbee596f5
malan88 <m@stdwtr.io>
2020-10-13 11:37:39 -0400

    Test the model acquiry

--- modeltexter.py
+++ modeltexter.py
@@ -1,4 +1,6 @@
 import os
+import boto3, urllib3
+import csv
 from twilio.rest import Client
 
 AUTH = os.environ['AUTH']
@@ -8,9 +10,21 @@ TO = os.environ['TO']
 
 CLIENT = Client(SID, AUTH)
 
+FIVETHIRTYEIGHT = 'https://projects.fivethirtyeight.com/'\
+    '2020-general-data/presidential_national_toplines_2020.csv'
+
+
+def getfiles():
+    http = urllib3.PoolManager()
+    resp = http.request('GET', FIVETHIRTYEIGHT)
+    five = resp.data.decode('utf-8')
+    return resp.status
+
 def text(message):
     message = CLIENT.messages.create(to=TO, from_=PHONE, body=message)
-    return message
+    stat = getfiles()
+
+    return [message.sid, stat]
 
 def main():
     data = text('test')
